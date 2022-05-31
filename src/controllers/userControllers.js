9636e7c5ea7568532f5ba682d0aabddca35d9dc8
Boris <boris@randebrock.com>
2022-05-31 20:30:55 +0200

    added userController and test

---
+++ src/controllers/userControllers.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const bcrypt = require("bcrypt");
+const User = require("../db/models/Users");
+
+const userRegister = async (req, res, next) => {
+  const { name, username, password } = req.body;
+  const user = await User.findOne({});
+
+  if (user) {
+    const error = new Error();
+    error.statusCode = 409;
+    error.message = "This user or password already exists! Please try again...";
+
+    next(error);
+  }
+  try {
+    const encryptedPassword = await bcrypt.hash(password, 10);
+    const NewUser = { name, username, password: encryptedPassword };
+
+    await User.create(NewUser);
+
+    res.status(201).json({ username });
+  } catch (error) {
+    error.statusCode = 400;
+    error.message = "Wrong user data... please try again!";
+
+    next(error);
+  }
+};
+
+module.exports = { userRegister };
