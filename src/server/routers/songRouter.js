6b482fbe1f99de07410ca9ae53fc2abf9257d8ca
Boris <boris@randebrock.com>
2022-06-08 18:26:42 +0200

    opened the doors

--- src/server/routers/songRouter.js
+++ src/server/routers/songRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { getSongs } = require("../../controller/songController");
 
 const songRouter = express.Router();
+
 songRouter.get("/songs", getSongs);
 
 module.exports = songRouter;
